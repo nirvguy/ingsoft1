f31c49603ae604ae4012c71067ce1802855c374b
Juan Cruz sosa <nirvguy@gmail.com>
2017-10-26 21:41:32 -0300

    Ejercicio 9: Refactoriza tests

--- 09-TusLibros.com/Python/tuslibros/test_carrito.py
+++ 09-TusLibros.com/Python/tuslibros/test_carrito.py
@@ -3,28 +3,27 @@ import unittest
 from carrito import Carrito
 
 class CarritoTest(unittest.TestCase):
+    def setUp(self):
+        self.libro = 'libro'
+        self.catalogo_con_un_solo_libro = set([self.libro])
+
     def test01_carrito_recien_creado_esta_vacio(self):
         carrito = Carrito(set())
-        libro = 'libro'
 
         self.assertTrue(carrito.vacio())
-        self.assertFalse(libro in carrito)
+        self.assertFalse(self.libro in carrito)
 
     def test02_carrito_con_un_elemento_no_esta_vacio(self):
-        libro = 'libro'
-        catalogo = set([libro])
-        carrito = Carrito(catalogo)
+        carrito = Carrito(self.catalogo_con_un_solo_libro)
 
-        carrito.agregar(libro)
+        carrito.agregar(self.libro)
 
         self.assertFalse(carrito.vacio())
-        self.assertTrue(libro in carrito)
+        self.assertTrue(self.libro in carrito)
 
     def test03_no_se_puede_agregar_un_producto_que_no_esta_en_el_catalogo(self):
-        libro = 'libro'
         otro_libro = 'otro libro'
-        catalogo = set([libro])
-        carrito = Carrito(catalogo)
+        carrito = Carrito(self.catalogo_con_un_solo_libro)
 
         try:
             carrito.agregar(otro_libro)
@@ -35,30 +34,25 @@ class CarritoTest(unittest.TestCase):
             self.assertTrue(otro_libro not in carrito)
 
     def test04_el_producto_agregado_una_vez_tiene_una_sola_unidad(self):
-        libro = 'libro'
-        catalogo = set([libro])
-        carrito = Carrito(catalogo)
+        carrito = Carrito(self.catalogo_con_un_solo_libro)
 
-        carrito.agregar(libro)
+        carrito.agregar(self.libro)
 
-        self.assertEqual(carrito.unidades(libro), 1)
+        self.assertEqual(carrito.unidades(self.libro), 1)
 
     def test05_el_producto_agregado_multiples_veces_tiene_esa_cantidad_de_unidades(self):
-        libro = 'libro'
-        catalogo = set([libro])
-        carrito = Carrito(catalogo)
+        carrito = Carrito(self.catalogo_con_un_solo_libro)
 
-        carrito.agregar(libro)
-        carrito.agregar(libro)
+        carrito.agregar(self.libro)
+        carrito.agregar(self.libro)
 
-        self.assertEqual(carrito.unidades(libro), 2)
+        self.assertEqual(carrito.unidades(self.libro), 2)
 
     def test06_no_se_puede_consultar_la_cantidad_de_unidades_de_un_producto_que_no_esta_en_el_carro(self):
-        libro = 'libro'
         carrito = Carrito(set())
 
         try:
-            unidades = carrito.unidades(libro)
+            unidades = carrito.unidades(self.libro)
             self.fail()
         except Exception as e:
             self.assertEqual(str(e), Carrito.PRODUCTO_NO_ESTA_EN_CATALOGO)
