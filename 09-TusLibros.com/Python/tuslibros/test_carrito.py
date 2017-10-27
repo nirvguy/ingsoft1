9213b77a5b1c634f7b26a9891f191c49ea238a03
Juan Cruz sosa <nirvguy@gmail.com>
2017-10-26 21:34:20 -0300

    Ejercicio 9: Agrega test de cantidad de elementos

--- 09-TusLibros.com/Python/tuslibros/test_carrito.py
+++ 09-TusLibros.com/Python/tuslibros/test_carrito.py
@@ -33,3 +33,33 @@ class CarritoTest(unittest.TestCase):
             self.assertEqual(str(e), Carrito.PRODUCTO_NO_ESTA_EN_CATALOGO)
             self.assertTrue(carrito.vacio())
             self.assertTrue(otro_libro not in carrito)
+
+    def test04_el_producto_agregado_una_vez_tiene_una_sola_unidad(self):
+        libro = 'libro'
+        catalogo = set([libro])
+        carrito = Carrito(catalogo)
+
+        carrito.agregar(libro)
+
+        self.assertEqual(carrito.unidades(libro), 1)
+
+    def test05_el_producto_agregado_multiples_veces_tiene_esa_cantidad_de_unidades(self):
+        libro = 'libro'
+        catalogo = set([libro])
+        carrito = Carrito(catalogo)
+
+        carrito.agregar(libro)
+        carrito.agregar(libro)
+
+        self.assertEqual(carrito.unidades(libro), 2)
+
+    def test06_no_se_puede_consultar_la_cantidad_de_unidades_de_un_producto_que_no_esta_en_el_carro(self):
+        libro = 'libro'
+        carrito = Carrito(set())
+
+        try:
+            unidades = carrito.unidades(libro)
+            self.fail()
+        except Exception as e:
+            self.assertEqual(str(e), Carrito.PRODUCTO_NO_ESTA_EN_CATALOGO)
+            self.assertTrue(carrito.vacio())
