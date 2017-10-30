69d176f30cb04452a680af02b9d1bb1eb34e06a0
Juan Cruz sosa <nirvguy@gmail.com>
2017-10-30 19:09:32 -0300

    Ejercicio 9: Refactoriza tests de carrito

--- 09-TusLibros.com/Python/tuslibros/test_carrito.py
+++ 09-TusLibros.com/Python/tuslibros/test_carrito.py
@@ -2,65 +2,60 @@ import unittest
 
 from carrito import Carrito
 
-libro = 'Libro'
-otro_libro = 'Otro Libro'
-un_tercer_libro = 'Un 3er Libro'
+LIBRO = 'Libro'
+OTRO_LIBRO = 'Otro Libro'
+UN_TERCER_LIBRO = 'Un 3er Libro'
 
-def crear_carrito_con_catalogo_vacio():
-    return Carrito(set())
-
-def crear_carrito_con_catalogo_de_un_solo_elemento():
-    return Carrito(set([libro]))
-
-def crear_carrito_con_catalogo_de_varios_elementos():
-    return Carrito(set([libro, otro_libro, un_tercer_libro]))
+CATALOGO_VACIO = {}
+CATALOGO_DE_UN_ELEMENTO = { LIBRO: 17 }
+CATALOGO_DE_MULTIPLES_ELEMENTOS = { LIBRO: 17, OTRO_LIBRO: 33, UN_TERCER_LIBRO: 42 }
 
 class CarritoTest(unittest.TestCase):
     def test01_carrito_recien_creado_esta_vacio(self):
-        carrito = crear_carrito_con_catalogo_vacio()
+        carrito = Carrito(CATALOGO_VACIO)
 
         self.assertTrue(carrito.vacio())
-        self.assertFalse(libro in carrito)
+        self.assertFalse(LIBRO in carrito)
 
     def test02_carrito_con_un_elemento_no_esta_vacio(self):
-        carrito = crear_carrito_con_catalogo_de_un_solo_elemento()
+        carrito = Carrito(CATALOGO_DE_UN_ELEMENTO)
 
-        carrito.agregar(libro)
+        carrito.agregar(LIBRO)
 
         self.assertFalse(carrito.vacio())
-        self.assertTrue(libro in carrito)
+        self.assertTrue(LIBRO in carrito)
 
     def test03_no_se_puede_agregar_un_producto_que_no_esta_en_el_catalogo(self):
-        carrito = crear_carrito_con_catalogo_de_un_solo_elemento()
+        carrito = Carrito(CATALOGO_DE_UN_ELEMENTO)
 
         try:
-            carrito.agregar(otro_libro)
+            carrito.agregar(OTRO_LIBRO)
             self.fail()
         except Exception as e:
             self.assertEqual(str(e), Carrito.PRODUCTO_NO_ESTA_EN_CATALOGO)
             self.assertTrue(carrito.vacio())
-            self.assertTrue(otro_libro not in carrito)
+            self.assertTrue(OTRO_LIBRO not in carrito)
 
     def test04_el_producto_agregado_una_vez_tiene_una_sola_unidad(self):
-        carrito = crear_carrito_con_catalogo_de_un_solo_elemento()
+        carrito = Carrito(CATALOGO_DE_UN_ELEMENTO)
 
-        carrito.agregar(libro)
+        carrito.agregar(LIBRO)
 
-        self.assertEqual(carrito.unidades(libro), 1)
+        self.assertEqual(carrito.unidades(LIBRO), 1)
 
     def test05_el_producto_agregado_multiples_veces_tiene_esa_cantidad_de_unidades(self):
-        carrito = crear_carrito_con_catalogo_de_un_solo_elemento()
+        carrito = Carrito(CATALOGO_DE_UN_ELEMENTO)
 
-        carrito.agregar(libro)
-        carrito.agregar(libro)
+        carrito.agregar(LIBRO)
+        carrito.agregar(LIBRO)
 
-        self.assertEqual(carrito.unidades(libro), 2)
+        self.assertEqual(carrito.unidades(LIBRO), 2)
 
     def test06_no_se_puede_consultar_la_cantidad_de_unidades_de_un_producto_que_no_esta_en_el_carro(self):
-        carrito = crear_carrito_con_catalogo_vacio()
+        carrito = Carrito(CATALOGO_VACIO)
 
         try:
-            unidades = carrito.unidades(libro)
+            unidades = carrito.unidades(LIBRO)
             self.fail()
         except Exception as e:
             self.assertEqual(str(e), Carrito.PRODUCTO_NO_ESTA_EN_CATALOGO)
